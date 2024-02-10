d8185ad8f61f23a6146b52c2728234412bb2f934
Oktay Çubukçu <ff3yz@ff3yzputer.local>
2024-02-10 22:29:11 +0300

    explained and updated

--- C05/ex01/ft_recursive_factorial.c
+++ C05/ex01/ft_recursive_factorial.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ocubukcu <ocubukcu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/10 22:27:24 by ocubukcu          #+#    #+#             */
+/*   Updated: 2024/02/10 22:27:25 by ocubukcu         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int ft_recursive_factorial(int nb)
 {
     // Eğer nb 1'den küçükse, faktöriyel hesaplanamaz, bu yüzden 0 döndürülür.
